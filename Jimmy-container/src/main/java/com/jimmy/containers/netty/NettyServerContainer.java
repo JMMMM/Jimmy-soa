97172230ff747b561b6fc87cc0ec58cb115b18b6
wujiaming <wujiaming@iplas.com.cn>
2018-01-11 15:04:01 +0800

    first commit

--- Jimmy-container/src/main/java/com/jimmy/containers/netty/NettyServerContainer.java
+++ Jimmy-container/src/main/java/com/jimmy/containers/netty/NettyServerContainer.java
@@ -1,8 +1,11 @@
 package com.jimmy.containers.netty;
 
 import com.jimmy.containers.Container;
+import com.jimmy.containers.netty.handler.NettyServerHandler;
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.PooledByteBufAllocator;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
@@ -28,9 +31,13 @@ public class NettyServerContainer implements Container {
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
                             IdleStateHandler handler = new IdleStateHandler(15, 0, 0);
-                            ch.pipeline().addLast(handler);
+                            ch.pipeline().addLast(handler, new NettyServerHandler());
                         }
-                    });
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
         }).start();
     }
 
---
+++ Jimmy-container/src/main/java/com/jimmy/containers/netty/handler/NettyServerHandler.java
@@ -0,0 +1,12 @@
+package com.jimmy.containers.netty.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+
+    }
+}
