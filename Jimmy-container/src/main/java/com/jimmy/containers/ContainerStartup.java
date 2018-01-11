1d477398d77e82e8ff67f4d31646b9fd7b5a3d24
wujiaming <wujiaming@iplas.com.cn>
2018-01-11 16:27:10 +0800

    12

--- Jimmy-container/src/main/java/com/jimmy/containers/ContainerStartup.java
+++ Jimmy-container/src/main/java/com/jimmy/containers/ContainerStartup.java
@@ -6,10 +6,8 @@ import org.slf4j.LoggerFactory;
 import java.util.ServiceLoader;
 
 public class ContainerStartup {
-    private static Logger logger = LoggerFactory.getLogger(ContainerStartup.class);
 
     public static void startup() {
-        logger.info("start to load containers .... ");
         //获取Container
         ServiceLoader<Container> containersServiceLoader = ServiceLoader.load(Container.class);
         containersServiceLoader.forEach(Container::start);
