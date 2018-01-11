8e0de6aff4d26ab7f698b0b9b8ea8927cfa1e92b
wujiaming <wujiaming@iplas.com.cn>
2018-01-11 16:41:22 +0800

    12

--- Jimmy-container/src/main/java/com/jimmy/containers/ContainerStartup.java
+++ Jimmy-container/src/main/java/com/jimmy/containers/ContainerStartup.java
@@ -9,7 +9,11 @@ public class ContainerStartup {
 
     public static void startup() {
         //获取Container
-        ServiceLoader<Container> containersServiceLoader = ServiceLoader.load(Container.class);
+        ServiceLoader<Container> containersServiceLoader = ServiceLoader.load(Container.class, ContainerStartup.class.getClassLoader());
+        for (Container container:containersServiceLoader){
+            System.out.println("cao");
+        }
         containersServiceLoader.forEach(Container::start);
     }
+
 }
